3d9d75595be2acb6ddd8590b29e37e7d7331140e
Patrick Carlson <carlsonp@iastate.edu>
2014-10-21 08:15:00 -0500

    settings file

---
+++ settings.py
@@ -0,0 +1,9 @@
+#settings.py
+
+XML_SERVER_URL = "https://yourhost.webex.com/WBXService/XMLService"
+SITEID = "123456"
+PASSWORD = "secret"
+WEBEX_IDS = ["webex1", "webex2"]
+DOWNLOAD_FOLDER = "/home/username/Desktop/webexdownloads/"
+#don't download sessions less than X days old, set to 0 to download all
+SESSION_LIMIT = 7 #default is one week
